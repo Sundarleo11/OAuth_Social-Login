1d4ab42150a7edc727471fff707a93624610b2db
Sundarleo11 <sundarrock1999@gmail.com>
2022-12-10 12:40:34 +0530

    addedPssportOAuth20

---
+++ passport/passport.js
@@ -0,0 +1,19 @@
+const passport = require("passport");
+
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: "821440983185-1ce5095qffs6l54nv98p1p91fic81bf3.apps.googleusercontent.com",
+        clientSecret: "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",
+       // callbackURL: "http://localhost:4000/auth/google/callback",
+        callbackURL: "http://localhost:4000/users/auth/google/callback"
+      },
+      ()=>{
+          //callback
+      }
+      )
+      
+);
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
-
+const passport = require("passport");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
+router.get(
+    "/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    }),
+    (req, res) => {
+      res.send("login with google");
+    }
+  );
 
 
 module.exports = router;
