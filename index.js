4b97c910797c84374ec50f61e99fa23de7804641
Sundarleo11 <sundarrock1999@gmail.com>
2022-12-10 19:33:11 +0530

    serializeUser&deserializeUser

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express=require('express');
 const mongoose=require('mongoose');
 const auth=require('./routes/auth');
 const passportConfig = require("./passport/passport");
-const passport = require("passport")
-const app=express()
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const app = express();
 
 
 //connect to the DB
@@ -11,10 +12,20 @@ mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
   console.log("DB CONNECTED")
 );
 
+app.use(
+  cookieSession({
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    keys: ["thisislcotokenkey"], // dotenv
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.set("view engine", "ejs");
 app.use("/auth", auth);
 
-app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.render("home");
--- passport/passport.js
+++ passport/passport.js
@@ -1,20 +1,51 @@
 const passport = require("passport");
-
+const User = require("../model/user");
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: "821440983185-1ce5095qffs6l54nv98p1p91fic81bf3.apps.googleusercontent.com",
-        clientSecret: "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",
-       // callbackURL: "http://localhost:4000/auth/google/callback",
-        callbackURL: "http://localhost:4000/auth/google/callback"
-      },
-      (accessToken, refreshToken, profile, next)=>{
-          //callback
-          console.log("User Profile",profile)
-      }
-      )
-      
+  new GoogleStrategy(
+    {
+      clientID: "821440983185-1ce5095qffs6l54nv98p1p91fic81bf3.apps.googleusercontent.com",
+      clientSecret: "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",
+      // callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL: "http://localhost:4000/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, next) => {
+      console.log("MY PROFILE", profile._json.email);
+      User.findOne({ email: profile._json.email }).then((user) => {
+        if (user) {
+          console.log("User already exits in DB", user);
+          next(null, user);
+          // cookietoken()
+        } else {
+          User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile._json.email
+          })
+            .then((user) => {
+              console.log("New User", user);
+              next(null, user);
+              // cookietoken()
+            })
+            .catch((err) => console.log(err));
+        }
+      });
+
+      //   next();
+    }
+  )
 );
+
